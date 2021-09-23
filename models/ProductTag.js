13623a725057a7ad0ea03fa78922a04cc2ca2150
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-23 17:15:32 -0600

    defined columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,39 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
+// define columns --added here
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: Datatypes.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: "product",
+        key: "ID",
+      },
+    },
+    tag_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "ID",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
