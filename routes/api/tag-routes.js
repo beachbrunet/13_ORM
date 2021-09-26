a07faefe1221e7247b9df43e7672d77f85863f22
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-25 21:32:29 -0600

    added missing code in tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,10 +15,10 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // find all tags
 // be sure to include its associated Product data
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -28,17 +28,18 @@ router.get("/", (req, res) => {
 
 // find a single tag by its `id`
 // be sure to include its associated Product data
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const tagData = await tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag}],
     });
     if (!tagData) {
       res.status(404).json({ message: "Not found with this id" });
       return;
     }
+    res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
@@ -83,7 +84,7 @@ router.delete("/:id", (req, res) => {
       where: { id: req.params.id },
     });
     if (!tagData) {
-      res.status(404).json({ message: "Not found with this id" });
+      res.status(404).json({ message: "No tag found with this id" });
       return;
     }
     res.status(200).json(tagData);
